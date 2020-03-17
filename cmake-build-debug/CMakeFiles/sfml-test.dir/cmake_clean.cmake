e859dfe3882142f6140277fad33486c67445cefd
Teodor G <grigor.teodor@gmail.com>
2020-03-17 20:28:48 +0000

    files imported

---
+++ cmake-build-debug/CMakeFiles/sfml-test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfml-test.dir/src/game/main.cpp.obj"
+  "libsfml-test.dll.a"
+  "sfml-test.exe"
+  "sfml-test.exe.manifest"
+  "sfml-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfml-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/game/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Green);
+
+    while (window.isOpen())
+    {
+        Event event{};
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
