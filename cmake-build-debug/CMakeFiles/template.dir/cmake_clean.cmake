fab979ec8cade559e7763a843b7f685d817fb032
Teodor G <grigor.teodor@gmail.com>
2020-03-17 21:28:47 +0000

    added header

---
+++ cmake-build-debug/CMakeFiles/template.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/template.dir/src/main/cpp/main.cpp.obj"
+  "libtemplate.dll.a"
+  "template.exe"
+  "template.exe.manifest"
+  "template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
