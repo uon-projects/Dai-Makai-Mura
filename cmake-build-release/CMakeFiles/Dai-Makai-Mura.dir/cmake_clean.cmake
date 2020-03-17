3f2ef3c39a37fec032ff8959990b1ac45d7fd0ca
Teodor G <grigor.teodor@gmail.com>
2020-03-17 21:38:20 +0000

    remove unwanted builds

---
+++ cmake-build-release/CMakeFiles/Dai-Makai-Mura.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dai-Makai-Mura.dir/src/main/cpp/main.cpp.obj"
+  "Dai-Makai-Mura.exe"
+  "Dai-Makai-Mura.exe.manifest"
+  "Dai-Makai-Mura.pdb"
+  "libDai-Makai-Mura.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dai-Makai-Mura.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
