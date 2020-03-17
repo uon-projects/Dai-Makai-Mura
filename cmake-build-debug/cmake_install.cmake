b2f0b6515479b7db96c7d7217d49a416302a85e9
Teodor G <grigor.teodor@gmail.com>
2020-03-17 23:18:06 +0000

    improved splash screen

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Da-Makai-Mura")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Dai-Makai-Mura")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
